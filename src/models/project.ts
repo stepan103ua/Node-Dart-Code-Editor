a9638e75d17c7aef5c1bf19358e4311dc817fd51
stepan103ua <stepan8877ua@gmail.com>
2023-04-20 19:04:01 +0300

    Create project model

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,13 +1,14 @@
 import { InferSchemaType, Schema, model } from 'mongoose';
 
 const projectSchema = new Schema({
-  name: { type: String },
+  name: { type: String, required: true },
   members: [
     {
       type: Schema.Types.ObjectId,
       ref: 'User',
     },
   ],
+  lastSaved: { type: Date, default: Date.now },
 });
 
 type Project = InferSchemaType<typeof projectSchema>;
